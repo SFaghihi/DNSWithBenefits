603cba93de11e4fc7db695148f07e2696fc40dd5
sfaghihi <faghihi@me.com>
2018-05-26 23:25:44 +0100

    Initial Commit

---
+++ DNS with Benefits/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DNS with Benefits
+//
+//  Created by Soroush Faghihi on 5/26/18.
+//  Copyright © 2018 sorco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
